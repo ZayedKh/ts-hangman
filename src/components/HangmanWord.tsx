71cd7c38faf734ff314cbb2c4f4a8251017a956a
Zayed <zayedkh02@gmail.com>
2025-03-01 21:07:51 +0000

    Finished up HangmanWord.tsx

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,28 @@
-
-
-export function HangmanWord(){}
+export function HangmanWord() {
+    const word: string = "test";
+    const guessedLetters: string[] = ["t"];
+    return <div style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+    }}
+    >
+        {word.split("").map((letter: string, index: number) => (
+            <span style={{
+                borderBottom: ".1em solid black",
+                margin: "0.1em",
+                marginTop: "1em",
+            }}
+                  key={index}>
+                <span style={{
+                    visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+                }}>
+                    {letter}
+                </span>
+            </span>
+        ))}
+    </div>
+}
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,3 +1,8 @@
+const KEYS: string[] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+export function Keyboard() {
 
 
-export function Keyboard(){}
+}
