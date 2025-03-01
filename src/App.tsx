7b7110a6fb3e7a5e916bf44bcac78f682327e624
Zayed <zayedkh02@gmail.com>
2025-03-01 19:39:50 +0000

    Added state to app, added word bank

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from "react";
+import words from "./wordList.json";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [solution, setSolution] = useState(() => {
+        return words[Math.floor(Math.random() * words.length)];
+    });
+
+    const [guessedLetters, setGuessedLetters] = useState<string>([]);
+
+    return (
+        <>
+            <h1>
+                Solution: {solution}
+            </h1>
+        </>
+    )
 }
 
 export default App
+
+/*
+    Need to track which letter the user had guessed.
+    Need to track whether the game has been won or lost.
+    Need to check if the current letter guess is in the word.
+ */
